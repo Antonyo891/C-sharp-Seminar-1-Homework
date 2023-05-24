e937f52d06c92180de89a8f5811144ae0d1e1ebf
Anton <B_atonyo_S@bk.ru>
2023-05-24 20:05:05 +0300

    Task 4 completed

---
+++ 4/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Выводит все четные от одного до N
+Console.Clear();
+Console.WriteLine("Введите число N");
+int Number = int.Parse(Console.ReadLine());
+while (Number<=1)
+{
+    Console.WriteLine("Введите число больше 1");
+    Number = int.Parse(Console.ReadLine());
+}
+for (int i = 1; i < Number + 1;i++){
+     if (i%2==0)
+     {
+        Console.Write($"{i} ");
+     }
+}
+Console.WriteLine();
