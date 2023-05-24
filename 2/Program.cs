3c70b4fe7a097a4f81595ff17fa0ce64137aef03
Anton <B_atonyo_S@bk.ru>
2023-05-24 19:42:28 +0300

    Task 2 completed

---
+++ 2/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Принимает на ввод три числа и выдает максимальное.
+
+int count = 0, index=0; 
+int[] array = new int[3];
+while(count<3) 
+{
+        Console.WriteLine($"Введите число {count+1}");
+        array[count] = int.Parse(Console.ReadLine());
+        count = count+1;
+}
+int iMax=0;
+while(index<3)
+{
+    if(array[index]>array[iMax])
+    {
+        iMax=index;
+    }
+    index++;
+}
+Console.WriteLine($"Макимальное число - номер {iMax+1} и равно {array[iMax]} ");
+
