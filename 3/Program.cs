25d6a36138183b5768c87e41e2a899817ba1254c
Anton <B_atonyo_S@bk.ru>
2023-05-24 19:51:54 +0300

    Task 3 comleted

---
+++ 3/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Является ли число четным
+Console.Clear();
+Console.WriteLine("Введите число");
+int Number = int.Parse(Console.ReadLine());
+if (Number%2==0)
+{
+    Console.WriteLine($"Число {Number} является четным");
+}
+else 
+{
+    Console.WriteLine($"Число {Number} является нечетным");
+}
