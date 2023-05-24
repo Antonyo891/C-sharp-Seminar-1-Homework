995f954d4228c1311095eed80911dba721f39298
Anton <B_atonyo_S@bk.ru>
2023-05-24 19:05:56 +0300

    1 task completed

--- 1/Program.cs
+++ 1/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*See https://aka.ms/new-console-template for more information
+// Задача №1 на вход принимаются два числа. Выводит какое число меньше.*/
+
+Console.Clear();
+Console.WriteLine("Введите первое число");
+int NumberA = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int NumberB = int.Parse(Console.ReadLine());
+if (NumberA > NumberB)
+{
+    Console.WriteLine($"Число {NumberA} больше числа {NumberB} ");
+}
+else 
+{
+   Console.WriteLine($"Число {NumberB} больше числа {NumberA} ");
+} 
